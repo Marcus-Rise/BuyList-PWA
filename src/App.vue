374d2246fa6bd1042925e62cd9e7a5f4f0d47fc0
Ilya Konstantinov <ilykonst95@gmail.com>
2019-11-11 19:51:38 +0400

    feat(HeaderCmpt.vue): add header component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template lang="pug">
     v-app
+        header-cmpt
         v-container
             transition
                 router-view
 </template>
+
+<script>
+    import { Vue, Component } from "vue-property-decorator";
+    import HeaderCmpt from "@/components/HeaderCmpt";
+
+    @Component({
+        components: { HeaderCmpt }
+    })
+    export default class App extends Vue {
+    }
+</script>
---
+++ src/components/HeaderCmpt.vue
@@ -0,0 +1,49 @@
+<template lang="pug">
+    header
+        v-app-bar
+            v-app-bar-nav-icon(@click="showMenu = true")
+            v-toolbar-title BuyList
+            v-spacer
+
+            v-menu(
+                left
+                bottom
+            )
+                template( v-slot:activator="{ on }")
+                    v-btn(icon v-on="on")
+                        v-icon fa-ellipsis-v
+
+                v-list
+                    v-list-item
+                        v-list-item-title Экспорт данных
+                    v-list-item
+                        v-list-item-title Импорт данных
+
+        v-navigation-drawer(
+            v-model="showMenu"
+            absolute
+            temporary
+            dark
+        )
+            v-list-item
+                v-list-item-avatar
+                    v-img(src="https://randomuser.me/api/portraits/men/78.jpg")
+                v-list-item-content BuyList
+
+            v-divider
+
+            v-list(dense)
+                v-list-item(link :to="{ name: 'productListArray' }")
+                    v-list-item-icon
+                        v-icon fa-list-ul
+                    v-list-item-content Списки продуктов
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from "vue-property-decorator";
+
+    @Component
+    export default class HeaderCmpt extends Vue {
+        public showMenu: boolean = true;
+    }
+</script>
