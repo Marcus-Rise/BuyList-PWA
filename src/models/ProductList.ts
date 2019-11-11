2802c15e8e0510aa34cb84ed11cc7aab996c1e7d
Ilya Konstantinov <i.konstantinov@astrovolga.ru>
2019-11-11 16:53:27 +0400

    feat(dto): add product list dto

--- src/models/ProductList.ts
+++ src/models/ProductList.ts
@@ -1,7 +1,24 @@
-export class ProductList {
-  constructor(public title: string = "", public id: number = 0) {}
+/* eslint-disable */
 
-  toString(): string {
-    return this.title;
-  }
+import {IProductListDTOJson} from "@/models/ProductListDTO";
+
+export class ProductList {
+    public title: string;
+    public id: number;
+
+    constructor(dto?: IProductListDTOJson);
+    constructor(title?: string, id?: number);
+    constructor(titleOrDTO: string | IProductListDTOJson = "", id: number = 0) {
+        if (typeof titleOrDTO !== "string") {
+            this.title = titleOrDTO.title;
+            this.id = titleOrDTO.id;
+        } else {
+            this.title = titleOrDTO;
+            this.id = id;
+        }
+    }
+
+    toString(): string {
+        return `id: ${this.id}; title: ${this.title}`
+    }
 }
---
+++ src/models/ProductListDTO.ts
@@ -0,0 +1,17 @@
+import {IDTO} from "@/core/IDTO";
+import {ProductList} from "@/models/ProductList";
+
+export class ProductListDTO implements IDTO {
+    constructor(private readonly model: ProductList) {
+    }
+
+    serialize(): IProductListDTOJson {
+        return this.model;
+    }
+
+}
+
+export interface IProductListDTOJson {
+    title: string;
+    id: number;
+}
