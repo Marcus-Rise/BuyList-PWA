9a31dfad54153042f33080c241ed161db7f8eeeb
Ilya Konstantinov <ilykonst95@gmail.com>
2019-11-11 20:23:58 +0400

    refactor(ProductListService.ts): fix dto using

--- src/services/implementations/ProductListService.ts
+++ src/services/implementations/ProductListService.ts
@@ -2,7 +2,7 @@ import { IProductListService } from "@/services/IProductListService";
 import { ProductList } from "@/models/ProductList";
 import { inject, injectable } from "tsyringe";
 import { IStorageService } from "@/services/IStorageService";
-import { ProductListDTO } from "@/models/ProductListDTO";
+import { IProductListDTOJson, ProductListDTO } from "@/models/ProductListDTO";
 
 @injectable()
 export class ProductListService implements IProductListService {
@@ -14,21 +14,21 @@ export class ProductListService implements IProductListService {
   ) {}
 
   async getAll(): Promise<ProductList[]> {
-    return (await this.storageService.getAll<ProductList>(this.table)).map(
+    return (await this.storageService.getAll<IProductListDTOJson>(this.table)).map(
       item => new ProductList(item)
     );
   }
 
   async save(item: ProductList): Promise<ProductList> {
     const lastId: number = (
-      await this.storageService.getAll<ProductList>(this.table)
+      await this.storageService.getAll<IProductListDTOJson>(this.table)
     ).reduce((latestId: number, current: ProductList) => {
       return current.id > latestId ? current.id : latestId;
     }, 0);
 
     item.id = lastId + 1;
 
-    return this.storageService.set<ProductList>(
+    return this.storageService.set<IProductListDTOJson>(
       this.table,
       item.id.toString(),
       new ProductListDTO(item).serialize()
@@ -37,7 +37,7 @@ export class ProductListService implements IProductListService {
 
   async get(id: number): Promise<ProductList> {
     return new ProductList(
-      await this.storageService.get<ProductList>(this.table, id.toString())
+      await this.storageService.get<IProductListDTOJson>(this.table, id.toString())
     );
   }
 }
--- src/services/implementations/ProductService.ts
+++ src/services/implementations/ProductService.ts
@@ -2,7 +2,7 @@ import { IProductService } from "@/services/IProductService";
 import { Product } from "@/models/Product";
 import { inject, injectable } from "tsyringe";
 import { IStorageService } from "@/services/IStorageService";
-import {ProductDTO} from "@/models/ProductDTO";
+import { IProductDTOJson, ProductDTO } from "@/models/ProductDTO";
 
 @injectable()
 export class ProductService implements IProductService {
@@ -15,19 +15,19 @@ export class ProductService implements IProductService {
 
   async get(id: number): Promise<Product> {
     return new Product(
-      await this.storageService.get<Product>(this.table, id.toString())
+      await this.storageService.get<IProductDTOJson>(this.table, id.toString())
     );
   }
 
   async getAll(): Promise<Product[]> {
-    return (await this.storageService.getAll<Product>(this.table)).map(
+    return (await this.storageService.getAll<IProductDTOJson>(this.table)).map(
       item => new Product(item)
     );
   }
 
   async save(item: Product): Promise<Product> {
     const lastId: number = (
-      await this.storageService.getAll<Product>(this.table)
+      await this.storageService.getAll<IProductDTOJson>(this.table)
     ).reduce((latestId: number, current: Product) => {
       return current.id > latestId ? current.id : latestId;
     }, 0);
@@ -35,7 +35,7 @@ export class ProductService implements IProductService {
     item.id = lastId + 1;
 
     return new Product(
-      await this.storageService.set<Product>(
+      await this.storageService.set<IProductDTOJson>(
         this.table,
         item.id.toString(),
         new ProductDTO(item).serialize()
