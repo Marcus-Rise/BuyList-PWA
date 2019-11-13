b94369ec18fccab3a7ac84cc827e3b0ba614520c
Ilya Konstantinov <ilykonst95@gmail.com>
2019-11-13 12:04:11 +0400

    Revert "build(pwa): set start_url"
    This reverts commit 6f5c8d18

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,4 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  publicPath: process.env.NODE_ENV === "production" ? "/buylist-pwa/" : "/",
-  pwa: {
-    manifestOptions: {
-      start_url: "buylist-pwa"
-    }
-  }
+  publicPath: process.env.NODE_ENV === "production" ? "/buylist-pwa/" : "/"
 };
