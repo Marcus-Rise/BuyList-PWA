3b33590ef1e00a96478ce40ebcbc0c7d2f7feb71
Ilya Konstantinov <ilykonst95@gmail.com>
2019-11-14 18:11:15 +0400

    ci(404.html): vue router fix for gitlab pages

--- src/main.ts
+++ src/main.ts
@@ -13,4 +13,11 @@ new Vue({
   router,
   vuetify,
   render: h => h(App),
+  created() {
+    if (sessionStorage.redirect) {
+      const redirect = sessionStorage.redirect;
+      delete sessionStorage.redirect;
+      this.$router.push(redirect);
+    }
+  }
 }).$mount("#app");
