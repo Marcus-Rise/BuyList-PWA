d136b3b244f1e1002c3d986788903a99a541793b
Ilya Konstantinov <ilykonst95@gmail.com>
2019-11-15 15:34:53 +0400

    build(vue.config.js): pwa start_url and img minification

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const ImageMinPlugin = require("imagemin-webpack-plugin").default;
+
 module.exports = {
   transpileDependencies: ["vuetify"],
   css: {
@@ -6,7 +8,18 @@ module.exports = {
   pwa: {
     name: "BuyList",
     manifestOptions: {
-      start_url: "/"
+      start_url: "./index.html"
     }
+  },
+  configureWebpack: {
+    plugins: [
+      // Make sure that the plugin is after any plugins that add images
+      new ImageMinPlugin({
+        disable: process.env.NODE_ENV !== "production", // Disable during development
+        pngquant: {
+          quality: "95-100"
+        }
+      })
+    ]
   }
 };
