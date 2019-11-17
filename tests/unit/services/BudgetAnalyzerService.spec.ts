04012c582e62d4865d63c70c68a2cb7d271fc6d5
Ilya Konstantinov <ilykonst95@gmail.com>
2019-11-17 12:02:41 +0400

    refactor(BudgetAnalyzerService.ts): optimize code

--- tests/unit/services/BudgetAnalyzerService.spec.ts
+++ tests/unit/services/BudgetAnalyzerService.spec.ts
@@ -22,5 +22,49 @@ describe("BudgetAnalyzerService", () => {
         new Product("third", 2, 250)
       ]);
     });
+
+    test("one product in limit", () => {
+      const products: Product[] = [new Product("second", 1, 150)];
+
+      expect(service.getBestChoice(products, 500)).toEqual([
+        new Product("second", 1, 150)
+      ]);
+    });
+
+    test("one product out of limit", () => {
+      const products: Product[] = [new Product("second", 1, 150)];
+
+      expect(service.getBestChoice(products, 100)).toEqual([]);
+    });
+
+    describe("comp building", () => {
+      const motherBoard: Product = new Product("motherboard", 5, 15000);
+      const cpu: Product = new Product("cpu", 10, 25000);
+      const memory: Product = new Product("memory", 3, 10000);
+      const gpu: Product = new Product("gpu", 7, 40000);
+
+      const products: Product[] = [motherBoard, cpu, memory, gpu];
+
+      test("30000", () => {
+        expect(service.getBestChoice(products, 30000)).toEqual([cpu]);
+      });
+
+      test("50000", () => {
+        expect(service.getBestChoice(products, 50000)).toEqual([
+          motherBoard,
+          cpu,
+          memory
+        ]);
+      });
+
+      test("90000", () => {
+        expect(service.getBestChoice(products, 90000)).toEqual([
+          motherBoard,
+          cpu,
+          memory,
+          gpu
+        ]);
+      });
+    });
   });
 });
