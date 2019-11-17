0364b382a5d0e3aed430799d57f7d6b691322530
Ilya Konstantinov <ilykonst95@gmail.com>
2019-11-17 13:34:05 +0400

    feat(AbstractValidatable.ts): extends models

--- src/models/ProductList.ts
+++ src/models/ProductList.ts
@@ -1,14 +1,18 @@
 /* eslint-disable */
 
 import {IProductListDTOJson} from "@/models/ProductListDTO";
+import { AbstractValidatable, ValidationErrorsObject } from "@/core/AbstractValidatable";
 
-export class ProductList {
+export class ProductList extends AbstractValidatable {
+    public errors: ValidationErrorsObject;
     public title: string;
     public id: number;
 
     constructor(dto?: IProductListDTOJson);
     constructor(title?: string, id?: number);
     constructor(titleOrDTO: string | IProductListDTOJson = "", id: number = 0) {
+        super();
+
         if (typeof titleOrDTO !== "string") {
             this.title = titleOrDTO.title;
             this.id = parseInt(titleOrDTO.id);
@@ -16,6 +20,10 @@ export class ProductList {
             this.title = titleOrDTO;
             this.id = id;
         }
+
+        this.errors = {
+            title: [],
+        };
     }
 
     toString(): string {
