96980a0308c16f260559577c4052d975a160fa98
Ilya Konstantinov <ilykonst95@gmail.com>
2019-11-23 10:12:57 +0400

    refactor(BudgetAnalyzerCmpt.vue): redesign

--- src/components/BudgetAnalyzerCmpt.vue
+++ src/components/BudgetAnalyzerCmpt.vue
@@ -1,25 +1,34 @@
 <template lang="pug">
     v-row
         v-col
-            v-form
+            v-form(@submit.prevent="match")
                 v-container
                     v-row
                         v-col(
                             cols="12"
-                            md="4"
+                            sm="4"
+                            xs="12"
                         )
-                            v-text-field(
+                            v-text-field.display-1(
                                 v-model="limit"
                                 label="Бюджет"
                                 type="number"
                                 min="1"
                                 required
+                                outlined
+                                prepend-icon="fa-wallet"
                             )
                         v-col(
                             cols="12"
-                            md="4"
+                            sm="3"
+                            xs="12"
                         )
-                            v-btn(color="primary" @click="match") Подобрать
+                            v-btn.title(
+                                type="submit"
+                                color="primary"
+                                block
+                                x-large
+                            ) Подобрать
 
                     v-row(v-if="newList")
                         v-list
@@ -31,32 +40,32 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
-import { container } from "tsyringe";
-import { Product } from "@/models/Product";
-import { IBudgetAnalyzerService } from "@/services/IBudgetAnalyzerService";
+    import { Component, Prop, Vue } from "vue-property-decorator";
+    import { container } from "tsyringe";
+    import { Product } from "@/models/Product";
+    import { IBudgetAnalyzerService } from "@/services/IBudgetAnalyzerService";
 
-@Component
-export default class BudgetAnalyzerCmpt extends Vue {
-  @Prop({ type: Array, required: true }) products!: Product[];
+    @Component
+    export default class BudgetAnalyzerCmpt extends Vue {
+        @Prop({ type: Array, required: true }) products!: Product[];
 
-  public limit: number = 0;
+        public limit: number = 0;
 
-  public newList: Product[] = [];
+        public newList: Product[] = [];
 
-  private readonly budgetAnalyzerService: IBudgetAnalyzerService = container.resolve(
-    "IBudgetAnalyzerService"
-  );
+        private readonly budgetAnalyzerService: IBudgetAnalyzerService = container.resolve(
+            "IBudgetAnalyzerService"
+        );
 
-  match(): void {
-    this.newList.length = 0;
+        match(): void {
+            this.newList.length = 0;
 
-    if (this.limit > 0 && this.products.length > 0) {
-      this.newList = this.budgetAnalyzerService.getBestChoice(
-        this.products,
-        this.limit
-      );
+            if (this.limit > 0 && this.products.length > 0) {
+                this.newList = this.budgetAnalyzerService.getBestChoice(
+                    this.products,
+                    this.limit
+                );
+            }
+        }
     }
-  }
-}
 </script>
