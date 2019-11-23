9132318a44992663de322c444ef462f9a80f03ea
Ilya Konstantinov <ilykonst95@gmail.com>
2019-11-23 13:35:12 +0400

    feat(DumpExportView.vue): init dump export window

--- src/views/Dump/DumpExportView.vue
+++ src/views/Dump/DumpExportView.vue
@@ -1,11 +1,40 @@
 <template lang="pug">
-    div wwwwwwww
+    v-container
+        v-row(align="center")
+            v-col
+                v-window.elevation-1(vertical)
+                    v-window-item
+                        v-container
+                            v-card(flat)
+                                v-card-text
+                                    v-row.mb-4(align="center")
+                                        p {{dumpObj}}
+            v-col(cols="12")
+                v-btn(
+                    color="primary"
+                    block
+                    large
+                ) Скопировать
 
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { IStorageDumpService } from "@/services/IStorageDumpService";
+import { container } from "tsyringe";
 
 @Component
-export default class DumpExportView extends Vue {}
+export default class DumpExportView extends Vue {
+  public dumpObj: object | null = null;
+
+  private readonly storageDumpService: IStorageDumpService = container.resolve(
+    "IStorageDumpService"
+  );
+
+  created(): void {
+    this.storageDumpService.export().then(obj => {
+      this.dumpObj = obj;
+    });
+  }
+}
 </script>
