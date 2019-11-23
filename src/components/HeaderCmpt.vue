c591d6172599c65525638d24d54c9d5e525b00b6
Ilya Konstantinov <ilykonst95@gmail.com>
2019-11-23 13:42:56 +0400

    feat(HeaderCmpt.vue): apply new dump windows

--- src/components/HeaderCmpt.vue
+++ src/components/HeaderCmpt.vue
@@ -45,52 +45,25 @@
                     v-list-item-icon
                         v-icon fa-list-ul
                     v-list-item-content Списки продуктов
-        v-overlay(:value="showOverlay")
-            component(v-if="storageDumpCmpt" :is="storageDumpCmpt")
-
-            v-row(justify="center" align="center")
-                v-btn(@click="showOverlay = false" color="primary") Закрыть
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import { container } from "tsyringe";
-import { IStorageDumpService } from "@/services/IStorageDumpService";
 
-@Component({
-  components: {
-    StorageImportDumpCmpt: () =>
-      import(
-        /*StorageImportDumpCmpt*/ "@/components/StorageImportDumpCmpt.vue"
-      ),
-    StorageExportDumpCmpt: () =>
-      import(/*StorageExportDumpCmpt*/ "@/components/StorageExportDumpCmpt.vue")
-  }
-})
+@Component
 export default class HeaderCmpt extends Vue {
   public showMenu: boolean = false;
-  public storageDumpCmpt:
-    | "StorageImportDumpCmpt"
-    | "StorageExportDumpCmpt"
-    | null = null;
-  public showOverlay: boolean = false;
 
-  private readonly storageDumpService: IStorageDumpService = container.resolve(
-    "IStorageDumpService"
-  );
-
-  storageDumpImport(obj: object): void {
-    this.storageDumpCmpt = "StorageImportDumpCmpt";
-    this.showOverlay = true;
+  storageDumpImport(): void {
+    this.$router.push({ name: "dumpImport" });
   }
 
   storageDumpExport(): void {
-    this.storageDumpCmpt = "StorageExportDumpCmpt";
-    this.showOverlay = true;
+    this.$router.push({ name: "dumpExport" });
   }
 
   storageDumpClear(): void {
-    this.storageDumpService.clear();
+    this.$router.push({ name: "dumpClear" });
   }
 }
 </script>
