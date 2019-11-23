8e0642dff3fe6948e2909263227fe7e98635da73
Ilya Konstantinov <ilykonst95@gmail.com>
2019-11-23 12:24:56 +0400

    refactor(ProductForm.vue): redesign

--- src/views/ProductForm.vue
+++ src/views/ProductForm.vue
@@ -16,7 +16,8 @@
                             v-row
                                 v-col(
                                     cols="12"
-                                    md="4"
+                                    xsm="12"
+                                    sm="4"
                                 )
                                     v-text-field(
                                         v-model="product.title"
@@ -26,102 +27,124 @@
                                     )
                                 v-col(
                                     cols="12"
-                                    md="4"
+                                    xsm="12"
+                                    sm="4"
                                 )
                                     v-text-field(
                                         type="number"
                                         min="0"
                                         v-model="product.priority"
+                                        @change="product.priority = parseInt(product.priority)"
                                         label="Приоритет"
                                         :error-messages="product.errors.priority"
                                         @input="product.errors.priority = []"
                                     )
+                                        template(v-slot:prepend)
+                                            v-icon(
+                                            ) fa-sort
                                 v-col(
                                     cols="12"
-                                    md="4"
+                                    xsm="12"
+                                    sm="4"
                                 )
                                     v-text-field(
                                         type="number"
                                         v-model="product.price"
+                                        @change="product.price = parseFloat(product.price)"
                                         min="1"
                                         label="Цена"
                                         :error-messages="product.errors.price"
                                         @input="product.errors.price = []"
                                     )
+                                        template(v-slot:prepend)
+                                            v-icon(
+                                            ) fa-ruble-sign
                             v-row
                                 v-col(
                                     cols="12"
-                                    md="4"
+                                    xsm="12"
+                                    sm="4"
                                 )
-                                    v-btn(@click="create()" color="primary") Сохранить
+                                    v-btn(
+                                        type="submit"
+                                        block
+                                        large
+                                        color="primary"
+                                    ) Сохранить
                                 v-col(
-                                    cols="12"
-                                    md="4"
                                     v-if="isEdit"
+                                    cols="12"
+                                    xsm="12"
+                                    sm="4"
                                 )
-                                    v-btn(@click="deleteItem()" color="accent") Удалить
+                                    v-btn(
+                                        @click="deleteItem()"
+                                        color="accent"
+                                        block
+                                        large
+                                    ) Удалить
 
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import { container } from "tsyringe";
-import { Product } from "@/models/Product";
-import { IProductService } from "@/services/IProductService";
-import { ProductDTO } from "@/models/ProductDTO";
+    import { Component, Vue } from "vue-property-decorator";
+    import { container } from "tsyringe";
+    import { Product } from "@/models/Product";
+    import { IProductService } from "@/services/IProductService";
+    import { ProductDTO } from "@/models/ProductDTO";
 
-@Component
-export default class ProductForm extends Vue {
-  get isEdit(): boolean {
-    return this.product.id !== new Product().id;
-  }
+    @Component
+    export default class ProductForm extends Vue {
+        get isEdit(): boolean {
+            return this.product.id !== new Product().id;
+        }
 
-  public product: Product = new Product();
+        public product: Product = new Product();
 
-  private readonly productService: IProductService = container.resolve(
-    "IProductService"
-  );
+        private readonly productService: IProductService = container.resolve(
+            "IProductService"
+        );
 
-  mounted() {
-    if (this.$route.params.id !== undefined) {
-      this.productService.get(parseInt(this.$route.params.id)).then(item => {
-        this.product = item;
-      });
+        mounted() {
+            if (this.$route.params.id !== undefined) {
+                this.productService.get(parseInt(this.$route.params.id)).then(item => {
+                    this.product = item;
+                });
+            }
+
+            if (this.$route.params.productListId !== undefined) {
+                this.product.productListId = parseInt(this.$route.params.productListId);
+            }
+        }
+
+        create(): void {
+            if (this.validate()) {
+                if (this.isEdit) {
+                    this.productService.update(this.product).then(() => {
+                        this.routerBack();
+                    });
+                } else {
+                    this.productService.save(this.product).then(() => {
+                        this.routerBack();
+                    });
+                }
+            }
+        }
+
+        deleteItem(): void {
+            this.productService.delete(this.product).then(() => {
+                this.$router.go(-1);
+            });
+        }
+
+        routerBack(): void {
+            this.$router.go(-1);
+        }
+
+        validate(): boolean {
+            this.product.clear();
+
+            return new ProductDTO(this.product).validate();
+        }
     }
-
-    if (this.$route.params.productListId !== undefined) {
-      this.product.productListId = parseInt(this.$route.params.productListId);
-    }
-  }
-
-  create(): void {
-    if (this.validate()) {
-      if (this.isEdit) {
-        this.productService.update(this.product).then(() => {
-          this.routerBack();
-        });
-      } else {
-        this.productService.save(this.product).then(() => {
-          this.routerBack();
-        });
-      }
-    }
-  }
-
-  deleteItem(): void {
-    this.productService.delete(this.product).then(() => {
-      this.$router.go(-1);
-    });
-  }
-
-  routerBack(): void {
-    this.$router.go(-1);
-  }
-
-  validate(): boolean {
-    this.product.clear();
-
-    return new ProductDTO(this.product).validate();
-  }
-}
 </script>
