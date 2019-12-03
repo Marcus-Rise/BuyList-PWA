686d4416dea5b79dc44856a8bf4f6c070f90c8b4
Ilya Konstantinov <ilykonst95@gmail.com>
2019-12-03 16:11:05 +0400

    build(vue.config.js): pwa settings

--- vue.config.js
+++ vue.config.js
@@ -17,9 +17,55 @@ module.exports = {
       swSrc: "./src/sw.js",
       swDest: "service-worker.js"
     },
-    manifestPath: "oldManifest",
+    // manifestPath: "oldManifest",
     manifestOptions: {
-      icons: []
+      icons: [
+        {
+          src: "/android-chrome-36x36.png",
+          sizes: "36x36",
+          type: "image/png"
+        },
+        {
+          src: "/android-chrome-48x48.png",
+          sizes: "48x48",
+          type: "image/png"
+        },
+        {
+          src: "/android-chrome-72x72.png",
+          sizes: "72x72",
+          type: "image/png"
+        },
+        {
+          src: "/android-chrome-96x96.png",
+          sizes: "96x96",
+          type: "image/png"
+        },
+        {
+          src: "/android-chrome-144x144.png",
+          sizes: "144x144",
+          type: "image/png"
+        },
+        {
+          src: "/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png"
+        },
+        {
+          src: "/android-chrome-256x256.png",
+          sizes: "256x256",
+          type: "image/png"
+        },
+        {
+          src: "/android-chrome-384x384.png",
+          sizes: "384x384",
+          type: "image/png"
+        },
+        {
+          src: "/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png"
+        }
+      ]
     }
   },
   configureWebpack: {
@@ -33,8 +79,8 @@ module.exports = {
       }),
       new FaviconsWebpackPlugin({
         logo: "./src/assets/logo.png",
-        prefix: "",
-        favicons: {
+        prefix: ""
+        /*favicons: {
           appName: "BuyList",
           appDescription: "BuyList App",
           developerName: packageJson.author,
@@ -45,7 +91,7 @@ module.exports = {
             coast: false,
             yandex: false
           }
-        }
+        }*/
       })
     ]
   }
