859a9aa243e3eb16c7ee8ffa842e9922d0a8fb52
Ilya Konstantinov <ilykonst95@gmail.com>
2019-12-08 15:14:15 +0400

    feat(FooterCmp.vue): add route link to changelog

--- src/components/FooterCmp.vue
+++ src/components/FooterCmp.vue
@@ -4,13 +4,34 @@
     )
         v-container
             v-row(dense)
-                v-col(xs="12" sm="12" md="4" cols="12")
-                    a(:href="supportEmail" style="color: inherit") Support email
-                v-col(xs="12" md="4" sm="12" cols="12")
-                    div версия: {{appService.version}}
-                v-col(xs="12" md="4" sm="12" cols="12")
+                v-col(
+                    xs="12"
+                    md="4"
+                    cols="12"
+                )
+                    a(
+                        :href="supportEmail"
+                        style="color: inherit"
+                    ) Support email
+                v-col(
+                    xs="12"
+                    md="4"
+                    cols="12"
+                )
+                    router-link(
+                        :to="{ name: 'changelog' }"
+                        style="color: inherit"
+                    ) версия: {{version}}
+                v-col(
+                    xs="12"
+                    md="4"
+                    cols="12"
+                )
                     | &copy; {{ year }} -&nbsp;
-                    a(href="https://marcus-rise.dev" style="color: inherit") {{appService.author}}
+                    a(
+                        :href="authorSiteUrl"
+                        style="color: inherit"
+                    ) {{author}}
 </template>
 
 <script lang="ts">
@@ -22,20 +43,29 @@
     export default class FooterCmt extends Vue {
         public readonly supportEmail: string =
             "mailto:incoming+marcus-rise-buylist-pwa-15263181-issue-@incoming.gitlab.com";
+        public readonly authorSiteUrl: string = "https://marcus-rise.dev";
 
         get year(): string {
             let val: string;
             if (this.yearNow >= this.yearStart) {
                 val = this.yearNow.toString();
-    } else {
-      val = `${this.yearStart} - ${this.yearNow}`;
+            } else {
+                val = `${this.yearStart} - ${this.yearNow}`;
+            }
+
+            return val;
+        }
+
+        get version(): string {
+            return this.appService.version;
+        }
+
+        get author(): string {
+            return this.appService.author;
+        }
+
+        private readonly yearStart: number = 2019;
+        private readonly yearNow: number = new Date().getFullYear();
+        private readonly appService: IAppService = container.resolve("IAppService");
     }
-
-    return val;
-  }
-
-  private readonly yearStart: number = 2019;
-  private readonly yearNow: number = new Date().getFullYear();
-  private readonly appService: IAppService = container.resolve("IAppService");
-}
 </script>
