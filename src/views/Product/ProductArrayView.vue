5ea10575ae65bded2d2dfbe07c9b78a505bfb89d
Ilya Konstantinov <ilykonst95@gmail.com>
2019-12-08 17:53:00 +0400

    feat(ProductArrayView.vue): new ProductArrayView.vue

--- src/views/Product/ProductArrayView.vue
+++ src/views/Product/ProductArrayView.vue
@@ -3,28 +3,6 @@
         v-if="!!productList"
         style="height: 100%"
     )
-        v-row(
-            v-if="productArray.length === 0"
-            style="height: 90%"
-            justify="center"
-            align="center"
-        )
-            v-col
-                v-card.mx-auto(
-                    max-width="344"
-                    outlined
-                )
-                    v-card-text
-                        p.display-1.text--primary(align="center") Вперед!
-
-                    v-card-actions
-                        v-btn(
-                            block
-                            x-large
-                            color="accent"
-                            @click="addItem"
-                        ) Создать продукт
-
         v-row(v-if="productArray.length > 0")
             v-col(sm="12")
                 v-card.mx-auto
@@ -52,11 +30,39 @@
                             v-model="searchQuery"
                         )
 
-                        editable-list-cmpt(
-                            :items="productsView"
-                            @edit="editItem"
-                            @delete="deleteItem"
+                        v-data-table(
+                            :headers="headers"
+                            :hide-default-header="productArray.length === 0"
+                            :hide-default-footer="productArray.length === 0"
+                            :items="filteredProductArray"
                         )
+                            template(v-slot:item.action="{ item }")
+                                v-icon.mr-6(
+                                    small
+                                    @click="editItem(item.id)"
+                                ) fa-pen
+
+                                v-icon.mr-2(
+                                    small
+                                    @click="deleteItem(item.id)"
+                                ) fa-trash
+
+                            template(v-slot:no-data)
+                                v-col
+                                    v-card.mx-auto(
+                                        max-width="344"
+                                        outlined
+                                    )
+                                        v-card-text
+                                            p.display-1.text--primary(align="center") Вперед!
+
+                                        v-card-actions
+                                            v-btn(
+                                                block
+                                                x-large
+                                                color="accent"
+                                                @click="addItem"
+                                            ) Создать продукт
             v-col(sm="12")
                 v-card.mx-auto
                     v-toolbar(
@@ -67,13 +73,17 @@
 
                     v-card-text
                         budget-analyzer-cmpt(:products="productArray")
+                            template(v-slot:new-list = "{ list }")
+                                v-data-table(
+                                    :headers="headers"
+                                    :items="list"
+                                )
 </template>
 
 <script lang="ts">
     import { Component, Vue } from "vue-property-decorator";
     import { container } from "tsyringe";
     import EditableListCmpt from "@/components/EditableListCmpt.vue";
-    import { IEditableListItem } from "@/components/IEditableListItem";
     import { Product } from "@/models/Product";
     import { IProductService } from "@/services/IProductService";
     import { ProductList } from "@/models/ProductList";
@@ -86,77 +96,83 @@
         components: { ListSearchFilterCmpt, BudgetAnalyzerCmpt, EditableListCmpt }
     })
     export default class ProductArrayView extends Vue {
-        get productsView(): IEditableListItem[] {
-            return this.filteredProductArray
-                .sort((a: Product, b: Product): number => {
-                    return b.priority - a.priority;
-                })
-      .map((item: Product) => {
-        return {
-          title: item.title,
-          key: item.id.toString(),
-          secondary: item.toStringFormatted()
-        };
-      });
-  }
+        get filteredProductArray(): Product[] {
+            return this.productArray.filter(item =>
+                item.title.includes(this.searchQuery ? this.searchQuery : "")
+            );
+        }
 
-  get filteredProductArray(): Product[] {
-    return this.productArray.filter(item =>
-      item.title.includes(this.searchQuery ? this.searchQuery : "")
-    );
-  }
+        public searchQuery: string = "";
+        public productArray: Product[] = [];
+        public productList: ProductList | null = null;
+        public readonly headers: Array<{
+            text: string,
+            value: string,
+            align?: "left",
+            sortable?: boolean,
+        }> = [
+            {
+                text: "Название",
+                value: "title"
+            },
+            {
+                text: "Приоритет",
+                value: "priority"
+            },
+            {
+                text: "Цена",
+                value: "price"
+            },
+            { text: "", value: "action", sortable: false }
+        ];
 
-  public searchQuery: string = "";
-  public productArray: Product[] = [];
-  public productList: ProductList | null = null;
+        private readonly productService: IProductService = container.resolve(
+            "IProductService"
+        );
+        private readonly productListService: IProductListService = container.resolve(
+            "IProductListService"
+        );
 
-  private readonly productService: IProductService = container.resolve(
-    "IProductService"
-  );
-  private readonly productListService: IProductListService = container.resolve(
-    "IProductListService"
-  );
+        created() {
+            this.productListService.get(parseInt(this.$route.params.productListId)).then(item => {
+                this.productList = item;
 
-  created() {
-      this.productListService.get(parseInt(this.$route.params.productListId)).then(item => {
-          this.productList = item;
+                this.getAll();
+            });
+        }
 
-          this.getAll();
-      });
-  }
+        getAll(): void {
+            if (this.productList) {
+                this.productService.getByList(this.productList.id).then(items => {
+                    this.productArray.length = 0;
+                    this.productArray.push(...items);
+                });
+            }
+        }
 
-  getAll(): void {
-    if (this.productList) {
-      this.productService.getByList(this.productList.id).then(items => {
-        this.productArray.length = 0;
-        this.productArray.push(...items);
-      });
+        addItem(): void {
+            this.$router.push({
+                name: "addProduct",
+                params: {
+                    productListId: this.productList ? this.productList.id.toString() : "0"
+                }
+            });
+        }
+
+        editItem(key: string): void {
+            this.$router.push({ name: "editProduct", params: { id: key } });
+        }
+
+        deleteItem(key: string): void {
+            this.productService.get(parseInt(key)).then(item => {
+                if (item) {
+                    this.productService.delete(item).then(() => {
+                        this.getAll();
+                    });
+                } else {
+                    throw new NotFoundException("product by " + key);
+                }
+            });
+        }
     }
-  }
-
-  addItem(): void {
-    this.$router.push({
-      name: "addProduct",
-      params: {
-        productListId: this.productList ? this.productList.id.toString() : "0"
-      }
-    });
-  }
-
-  editItem(key: string): void {
-    this.$router.push({ name: "editProduct", params: { id: key } });
-  }
-
-  deleteItem(key: string): void {
-    this.productService.get(parseInt(key)).then(item => {
-      if (item) {
-        this.productService.delete(item).then(() => {
-          this.getAll();
-        });
-      } else {
-        throw new NotFoundException("product by " + key);
-      }
-    });
-  }
-}
 </script>
