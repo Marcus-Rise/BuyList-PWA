7bf81799dafad3212ba005f549f5b9a165f911c5
Ilya Konstantinov <ilykonst95@gmail.com>
2019-12-08 14:33:12 +0400

    build(vue.config.js): add CHANGELOG.md content to env var

--- vue.config.js
+++ vue.config.js
@@ -2,97 +2,112 @@ const ImageMinPlugin = require("imagemin-webpack-plugin").default;
 const packageJson = require("./package");
 const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
+const markdown = require("markdown-js");
+const fs = require("fs");
+const str = fs.readFileSync("CHANGELOG.md", "utf8");
+const result = markdown.makeHtml(str);
+
 process.env.VUE_APP_VERSION = packageJson.version;
 process.env.VUE_APP_AUTHOR = packageJson.author;
+process.env.VUE_APP_CHANGELOG = result;
 
 module.exports = {
-  transpileDependencies: ["vuetify"],
-  css: {
-    extract: { ignoreOrder: true }
-  },
-  pwa: {
-    name: "BuyList",
-    workboxPluginMode: "InjectManifest",
-    workboxOptions: {
-      swSrc: "./src/sw.js",
-      swDest: "service-worker.js"
+    transpileDependencies: ["vuetify"],
+    css: {
+        extract: { ignoreOrder: true }
     },
-    // manifestPath: "oldManifest",
-    manifestOptions: {
-      icons: [
-        {
-          src: "/android-chrome-36x36.png",
-          sizes: "36x36",
-          type: "image/png"
+    pwa: {
+        name: "BuyList",
+        workboxPluginMode: "InjectManifest",
+        workboxOptions: {
+            swSrc: "./src/sw.js",
+            swDest: "service-worker.js"
         },
-        {
-          src: "/android-chrome-48x48.png",
-          sizes: "48x48",
-          type: "image/png"
-        },
-        {
-          src: "/android-chrome-72x72.png",
-          sizes: "72x72",
-          type: "image/png"
-        },
-        {
-          src: "/android-chrome-96x96.png",
-          sizes: "96x96",
-          type: "image/png"
-        },
-        {
-          src: "/android-chrome-144x144.png",
-          sizes: "144x144",
-          type: "image/png"
-        },
-        {
-          src: "/android-chrome-192x192.png",
-          sizes: "192x192",
-          type: "image/png"
-        },
-        {
-          src: "/android-chrome-256x256.png",
-          sizes: "256x256",
-          type: "image/png"
-        },
-        {
-          src: "/android-chrome-384x384.png",
-          sizes: "384x384",
-          type: "image/png"
-        },
-        {
-          src: "/android-chrome-512x512.png",
-          sizes: "512x512",
-          type: "image/png"
+        // manifestPath: "oldManifest",
+        manifestOptions: {
+            icons: [
+                {
+                    src: "/android-chrome-36x36.png",
+                    sizes: "36x36",
+                    type: "image/png"
+                },
+                {
+                    src: "/android-chrome-48x48.png",
+                    sizes: "48x48",
+                    type: "image/png"
+                },
+                {
+                    src: "/android-chrome-72x72.png",
+                    sizes: "72x72",
+                    type: "image/png"
+                },
+                {
+                    src: "/android-chrome-96x96.png",
+                    sizes: "96x96",
+                    type: "image/png"
+                },
+                {
+                    src: "/android-chrome-144x144.png",
+                    sizes: "144x144",
+                    type: "image/png"
+                },
+                {
+                    src: "/android-chrome-192x192.png",
+                    sizes: "192x192",
+                    type: "image/png"
+                },
+                {
+                    src: "/android-chrome-256x256.png",
+                    sizes: "256x256",
+                    type: "image/png"
+                },
+                {
+                    src: "/android-chrome-384x384.png",
+                    sizes: "384x384",
+                    type: "image/png"
+                },
+                {
+                    src: "/android-chrome-512x512.png",
+                    sizes: "512x512",
+                    type: "image/png"
+                }
+            ]
         }
-      ]
+    },
+    configureWebpack: (config) => {
+        config.module.rules.push(
+            {
+                test: /\.md$/,
+                use: "raw-loader"
+            }
+        );
+
+        if (process.env.NODE_ENV === "production") {
+            config.plugins.push(...[
+                // Make sure that the plugin is after any plugins that add images
+                new ImageMinPlugin({
+                    disable: process.env.NODE_ENV !== "production", // Disable during development
+                    pngquant: {
+                        quality: "95-100"
+                    }
+                }),
+                new FaviconsWebpackPlugin({
+                    logo: "./src/assets/logo.png",
+                    prefix: ""
+                    /*favicons: {
+                      appName: "BuyList",
+                      appDescription: "BuyList App",
+                      developerName: packageJson.author,
+                      developerURL: "https://marcus-rise.dev", // prevent retrieving from the nearest package.json
+                      background: "#ddd",
+                      theme_color: "#333",
+                      icons: {
+                        coast: false,
+                        yandex: false
+                      }
+                    }*/
+                })
+            ]);
+        }
     }
-  },
-  configureWebpack: {
-    plugins: [
-      // Make sure that the plugin is after any plugins that add images
-      new ImageMinPlugin({
-        disable: process.env.NODE_ENV !== "production", // Disable during development
-        pngquant: {
-          quality: "95-100"
-        }
-      }),
-      new FaviconsWebpackPlugin({
-        logo: "./src/assets/logo.png",
-        prefix: ""
-        /*favicons: {
-          appName: "BuyList",
-          appDescription: "BuyList App",
-          developerName: packageJson.author,
-          developerURL: "https://marcus-rise.dev", // prevent retrieving from the nearest package.json
-          background: "#ddd",
-          theme_color: "#333",
-          icons: {
-            coast: false,
-            yandex: false
-          }
-        }*/
-      })
-    ]
-  }
 };
