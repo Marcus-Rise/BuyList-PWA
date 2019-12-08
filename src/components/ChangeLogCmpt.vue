ed10f53eaa3815cec720a09ff8725d506bbdfcd0
Ilya Konstantinov <ilykonst95@gmail.com>
2019-12-08 14:33:54 +0400

    feat(ChangeLogCmpt.vue): init new component

---
+++ src/components/ChangeLogCmpt.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+    div(v-html="source")
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from "vue-property-decorator";
+    import { container } from "tsyringe";
+    import { IAppService } from "@/services/IAppService";
+
+    @Component
+    export default class ChangeLogCmpt extends Vue {
+        public source: string = container.resolve<IAppService>("IAppService").changeLog || "awd";
+    }
+</script>
